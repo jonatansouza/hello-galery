209a4d08e239881645ed2987215995d12f192a9d
Jonatan <jonatangd.souza@gmail.com>
2019-06-30 12:20:36 -0300

    add search hero on provider

--- src/app/core/services/hero-provider.service.ts
+++ src/app/core/services/hero-provider.service.ts
@@ -10,16 +10,21 @@ import { Observable } from 'rxjs';
 export class HeroProviderService {
   private MAX_HERO_ID :number;
   private apiUrl: string;
+  private url: string;
   constructor(private http: HttpClient) {
     this.apiUrl = environment.HERO_API.URL;
     this.MAX_HERO_ID = environment.CONFIG.MAX_HERO_ID || 1;
+    this.url = `${this.apiUrl}${environment.HERO_API.TOKEN}/`
   }
 
   public getHeroById(id: number): Observable<Hero>{
-    return this.http.get<Hero>(`${this.apiUrl}${environment.HERO_API.TOKEN}/${id}`);
+    return this.http.get<Hero>(`${this.url}/${id}`);
   }
   public getHeroRandon(){
     const t = () => Math.floor(Math.random() * this.MAX_HERO_ID) + 1;
     return this.getHeroById(t());
+  }
+  public searchHero(name: string) {
+    return this.http.get<Hero>(`${this.url}/search/${name}`);
   }
 }
