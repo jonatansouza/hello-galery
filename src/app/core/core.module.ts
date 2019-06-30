9bbe2dcbddd61c042a73f14f2166cb8eefea40db
Jonatan <jonatangd.souza@gmail.com>
2019-06-30 16:48:08 -0300

    add firebase store

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,10 @@ import { NavbarComponent } from './headers/navbar/navbar.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 const providers = [
   HeroProviderService
@@ -16,9 +20,12 @@ const providers = [
     CommonModule,
     HttpClientModule,
     RouterModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule.enablePersistence()
   ],
-  exports: [NavbarComponent, RouterModule],
+  exports: [NavbarComponent, RouterModule, AngularFireModule, AngularFireAuthModule, AngularFirestoreModule],
   providers: providers
 })
 export class CoreModule { 
