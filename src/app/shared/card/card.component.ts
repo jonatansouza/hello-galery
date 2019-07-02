6e43f910e1c4c3d51247858fe63c201e3611a19b
Jonatan <jonatan.gall@ibm.com>
2019-07-01 23:33:23 -0300

    add logic to add and remove favorites on card

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,6 +1,7 @@
+import { StorageProviderService } from './../../core/services/storage-provider.service';
 import { GlobalSettingsService } from './../../core/services/global-settings.service';
 import { Hero } from './../interfaces/hero';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import { Theme } from '../interfaces/theme';
 
 @Component({
@@ -11,10 +12,33 @@ import { Theme } from '../interfaces/theme';
 export class CardComponent implements OnInit {
   @Input() hero: Hero
   @Input() theme: Theme;
+  @Output() updateHero: boolean;
+  public disableBtn:boolean;
+  private favoritesIds: string[];
+  constructor(private storageProvider :StorageProviderService) {
+    
+  }
   ngOnInit() {
+    this.storageProvider.getUserFavorites()
+      .subscribe((favorites) => {
+        this.favoritesIds = favorites;
+        if(this.favoritesIds.includes(this.hero.id)){
+          this.hero.favorite = true;
+          return;
+        }
+        this.hero.favorite = false;
+      })
   }
   updateFavorite() {
-    this.hero.favorite = !this.hero.favorite;
+    this.disableBtn = true;
+    const idx = this.favoritesIds.indexOf(this.hero.id)
+    if(this.hero.favorite){
+      this.favoritesIds.splice(idx, 1);
+    } else {
+      this.favoritesIds.push(this.hero.id);
+    }
+    this.storageProvider.updateUserFavorites(this.favoritesIds)
+      .finally(() => this.disableBtn = false)
   }
   formatField(property: string){
     return property === "null" ? "-" : property;
